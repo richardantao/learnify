df8b96193b2c1b43a156ccb40b29e93c02f5c50b
Richard Antao <richardmantao@gmail.com>
2019-11-29 11:21:17 -0500

    Added POST data

---
+++ test/data/create/createTerm.js
@@ -0,0 +1,36 @@
+export const term = {
+    _id: ObjectId(),
+    title: "Fall 2019",
+    date: {
+        start: "2019-09-07",
+        end: "2019-12-18"
+    }
+};
+
+export const noTitle = {
+    _id: ObjectId(),
+    title: "",
+    date: {
+        start: "2019-09-07",
+        end: "2019-12-18"
+    }
+};
+
+export const noStart = {
+    _id: ObjectId(),
+    title: "Fall 2019",
+    date: {
+        start: "",
+        end: "2019-12-18"
+    }
+};
+
+export const noEnd = {
+    _id: ObjectId(),
+    title: "Fall 2019",
+    date: {
+        start: "2019-09-07",
+        end: ""
+    }
+};
+
