c77ebb7b1b39f24b591551911cda42af967ac2a6
Richard Antao <richardmantao@gmail.com>
2019-12-07 00:45:32 -0500

    Test validation next

--- routes/years.route.js
+++ routes/years.route.js
@@ -4,15 +4,15 @@ const controller = require("../controllers/years.controller");
 const auth = require("../middleware/auth.middleware");
 const validation = require("../middleware/validation/years.validation");
 
-router.post("/", auth, validation, controller.create);
+router.post("/years", auth, validation, controller.create);
 
-router.get("/", auth, controller.read);
+router.get("/years", auth, controller.read);
 
-router.get("/:yearId", auth, controller.edit);
+router.get("/years/:yearId", auth, controller.edit);
 
-router.put("/:yearId", auth, validation, controller.update);
+router.put("/years/:yearId", auth, validation, controller.update);
 
-router.delete("/:yearId", auth, controller.delete);
+router.delete("/years/:yearId", auth, controller.delete);
 
 module.exports = router;
 
