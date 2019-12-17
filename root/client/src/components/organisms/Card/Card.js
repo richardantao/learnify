7e8b931d91aefd995d3ee94a1d939fab53d0e0fb
Richard Antao <richardmantao@gmail.com>
2019-12-16 23:16:31 -0500

    Created Card

--- root/client/src/components/organisms/Card/Card.js
+++ root/client/src/components/organisms/Card/Card.js
@@ -9,7 +9,16 @@ export default class Card extends Component {
         return (
             <Row>
                 <Col>
-                    
+                    <div className="card">
+                        <img src={this.props.src} className="headshot" alt={this.props.alt}/>
+                        <div>
+                            <h2>{this.props.person}</h2>
+                            <h3>{this.props.title}</h3>
+                            <p>
+                                {this.props.description}
+                            </p>
+                        </div>
+                    </div>
                 </Col>
             </Row>
         );
