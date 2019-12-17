df7b68e240a768732a5f3d606b06ebcdce5796d8
Richard Antao <richardmantao@gmail.com>
2019-12-17 02:09:48 -0500

    Initialized server

--- app/server.js
+++ app/server.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.use("/v1", require("./routes"));
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
--- team/server.js
+++ team/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
-
 const express = require("express");
+
 const app = express();
+const port = process.env.PORT || 3001;
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`)
