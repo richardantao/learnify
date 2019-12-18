67785a0e9da853b0c72a060721a29701a202729a
Richard Antao <richardmantao@gmail.com>
2019-12-18 16:18:50 -0500

    Updated packages

--- blog/assets/scripts/global.js
+++
@@ -1,6 +0,0 @@
-// render copyright statement inside footer after window loads 
-window.onload = function copyrightYear() {
-    let year = new Date().getFullYear();
-
-    document.querySelector("#copyright p").innerHTML = "Copyright <i class='far fa-copyright'></i> " + year + " Learnify. All rights reserved.";
-}
