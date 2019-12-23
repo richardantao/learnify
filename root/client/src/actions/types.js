302f8e02054f9623fa686f01dde5da46ce616a86
Richard Antao <richardmantao@gmail.com>
2019-12-23 01:07:11 -0500

    Updated types

--- root/client/src/actions/types.js
+++ root/client/src/actions/types.js
@@ -1,7 +1,10 @@
 export const PROCESSING_FORM = "PROCESSING_FORM";
+
 export const POST_INVITE = "POST_INVITE";
 export const INVITE_ERROR = "INVITE_ERROR";
+
 export const POST_CONTACT = "POST_CONTACT";
 export const CONTACT_ERROR = "CONTACT_ERROR";
+
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
