0a3017b3dc6eb4f97cc17b9b313319e6502615a3
Richard Antao <richardmantao@gmail.com>
2020-01-03 15:26:52 -0500

    Implemented hamburger overlay for mobile screens

--- client/src/components/public/docs/organisms/SideNav/SideNav.js
+++ client/src/components/public/docs/organisms/SideNav/SideNav.js
@@ -1,31 +1,80 @@
 import React, { Component } from "react";
 
+import SideOverlay from "../SideOverlay";
+
 import "../../Docs.scss";
 
 export default class SideNav extends Component {
     state = {
-        
+        hamburger: false,
+        width: window.innerWidth
+    };
+
+    trackWindowWidth = () => {
+        this.setState({
+            width: window.innerWidth
+        });
     };
 
     componentDidMount() {
-    
+        const { width } = this.state;
+        window.addEventListener("resize", this.trackWindowWidth);
+
+        if(width < 768) {
+            this.setState({
+                hamburger: true
+            });
+        } else {
+            this.setState({
+                hamburger: false
+            });
+        };
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.trackWindowWidth);
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        const { width } = this.state;
+
+        if(width !== prevState.width) {
+            if(width < 768) {
+                this.setState({
+                    hamburger: true
+                });
+            } else {
+                this.setState({
+                    hamburger: false
+                });
+            };
+        }; 
     };
 
     render() {
+        const { hamburger } = this.state;
 
         return (
             <header role="banner">
                 <nav className="side-nav" style={{height: this.props.siblingHeight}}>
                     <div>
-                        <h3>Explore</h3>
-                            <a href="/docs">What is Learnify?</a>
-                            <a href="/docs/api">Developers</a>
-                        <h3>Resources</h3>
-                            <a href="/docs/terms">Terms of Service</a>
-                            <a href="/docs/privacy">Privacy Policy</a>
-                            <a href="/docs/cookies">Cookie Policy</a>
-                        <h3>System</h3>
-                            <a href="/docs/changelog">Changelog</a>
+                        { hamburger ? (
+                            <SideOverlay/>
+                        ):
+                            <>
+                                <h3>Explore</h3>
+                                    <a href="/docs">What is Learnify?</a>
+                                    <a href="/docs/api">Developers</a>
+                                <h3>Resources</h3>
+                                    <a href="/docs/terms">Terms of Service</a>
+                                    <a href="/docs/privacy">Privacy Policy</a>
+                                    <a href="/docs/cookies">Cookie Policy</a>
+                                    <a href="/docs/sitemap">Sitemap</a>
+                                <h3>System</h3>
+                                    <a href="/docs/changelog">Changelog</a>
+                                    <a href="/docs/status">Status</a>
+                            </>
+                        }
                     </div>
                 </nav>
             </header>
