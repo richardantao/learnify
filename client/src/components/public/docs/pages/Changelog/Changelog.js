db4503b4cb510d5a9e229de66c11a7b82a74aec2
Richard Antao <richardmantao@gmail.com>
2020-01-03 22:35:00 -0500

    Added upcoming releases

--- client/src/components/public/docs/pages/Changelog/Changelog.js
+++ client/src/components/public/docs/pages/Changelog/Changelog.js
@@ -87,32 +87,56 @@ class Changelog extends Component {
                             <Input type="checkbox" id="minor" name="minor" checked={showMinors} onChange={this.toggleMinor}/> <span>Minor Versions</span>
                             <Input type="checkbox" id="patch" name="patch" checked={showPatches} onChange={this.togglePatch}/> <span>Patch Versions</span>
                         </header>
-                        <br/>    
+                        <br/>
+                        { showMinors ? (
+                            <div className="minor">
+                                <h2>v1.1.0-beta</h2>
+                                <h5>Unreleased</h5>
+                                <p>
+                                    This release will focus on performance optimization and reflecting on the feedback 
+                                    received from our first beta test sprint. We'll also be working away on our 
+                                    first post&ndash;launch feature. Stay tuned for updates.
+                                </p>
+                                <hr/>
+                            </div>
+                        ): null }    
                         { showMajors ? (
                             <div className="major">
-                                <h2>v1.0.0</h2>
-                                <h5>January x, 2020</h5>
+                                <h2>v1.0.0-beta</h2>
+                                <h5>January 8, 2020</h5>
                                 <p>
-                                    The app is live! We're kicking off our launch with a closed beta, to receive initial feedback on quality and bugs.
-                                    You can request an invite at <a href="https://learnify.ca/">https://learnify.ca</a>. Further updates can be expected in the near future.
+                                    The app is live! We're kicking off our launch with a closed beta, 
+                                    to receive initial feedback on quality and bugs. 
+                                    You can request an invite at <a href="https://learnify.ca/">https://learnify.ca</a>. 
                                 </p>
                                 <hr/>
                             </div>
                         ): null}
                         { showMinors ? (
-                            <div>
+                            <div className="minor">
                                 <h2>We're starting a changelog</h2>
-                                <h5>December x, 2019</h5>
+                                <h5>January 4, 2020</h5>
                                 <p>
-                                    Big news today, we're starting a public changelog so you're always up to date with every update, improvement, and fix that is made in Learnify.
+                                    Big news today, we're starting a public changelog so you're always up 
+                                    to date with every update, improvement, and fix that is made in Learnify.
                                 </p>
                                 <p>
-                                    Even though we're constantly working on how we can make Learnify serve you better, sometimes it may seem that not much is happening. This changelog is here to improve that very important part 
-                                    of communication between you and us.
+                                    Even though we're constantly working on how we can make Learnify serve you better, 
+                                    sometimes it may seem that not much is happening. This changelog is here to improve 
+                                    that very important part of communication between you and us.
                                 </p>
                                 <hr/>
                             </div>
                         ): null}
+                        { showMajors ? (
+                            <div className="major">
+                                <h2>v0.1.0</h2>
+                                <h5>January 1, 2019</h5>
+                                <p>
+                                    Initial development.
+                                </p>
+                            </div>
+                        ): null }
                     </main>
                     <Footer/>
                 </div>
