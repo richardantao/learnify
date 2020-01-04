dd2eb1fcfc1fef0e736282cea76999550a0f64ba
Richard Antao <richardmantao@gmail.com>
2020-01-03 22:41:50 -0500

    Registered service worker for production

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,18 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-// import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 const Index = () => <App/>
 
 const customHistory = createBrowserHistory();
 
-render(
+render (
 	<Router history={customHistory}>
 		<Index />
 	</Router>,
 	document.querySelector("#learnify")
 );
+
+// register service worker
+serviceWorker.register();
