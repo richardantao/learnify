6f229617d7cfac06e1566aac1225ed6f89317fa0
Richard Antao <richardmantao@gmail.com>
2020-01-03 22:32:10 -0500

    Changed logo jpg name

--- client/src/components/beta/global/Logo/Logo.js
+++ client/src/components/beta/global/Logo/Logo.js
@@ -1,23 +1,10 @@
-import React, { Component } from "react";
-import logo from "./tutee-min.jpg"
+import React from "react";
+
+import logo from "./learnify-min.jpg"
 import "./Logo.scss";
 
-export default class Logo extends Component {
-	constructor(props) {
-		super(props);
+const Logo = () => {
+	return <a href="/beta/dashboard"><img src={logo} alt="Learnify logo and mascot" className="logo"/></a>	
+};
 
-		this.state = {
-			isLoading: true
-		}
-	}
-	
-	componentDidMount() {
-		this.setState({
-			isLoading: false
-		});
-	}
-
-	render() {
-		return <a href="/app/dashboard"><img src={logo} alt="Learnify logo and mascot" className="logo"/></a>
-	}
-}
+export default Logo;
