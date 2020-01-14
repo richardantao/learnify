4eab998d3668e5ab37ecc833a24d46db3ad9fc81
Richard Antao <richardmantao@gmail.com>
2020-01-13 20:22:49 -0500

    Removed storage middleware

--- routes/team.js
+++ routes/team.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 const validation = require("../middleware/validation/team");
-const storage = require("../middleware/storage/team");
 const controller = require("../controllers/team");
 
-router.post("/applications/backend", validation.tech, storage.multer.single("resume"), storage.uploadToGCP, controller.backend);
+router.post("/applications/backend", validation.tech, controller.backend);
 
-router.post("/applications/creator", validation.nontech, storage.multer.single("resume"), storage.uploadToGCP, controller.creator);
+router.post("/applications/creator", /*validation.nontech,*/  controller.creator);
 
-router.post("/applications/designer", validation.nontech, storage.multer.single("resume"), storage.uploadToGCP, controller.designer);
+router.post("/applications/designer", validation.nontech,  controller.designer);
 
-router.post("/applications/frontend", validation.tech, storage.multer.single("resume"), storage.uploadToGCP, controller.frontend);
+router.post("/applications/frontend", validation.tech,  controller.frontend);
 
-router.post("/applications/marketer", validation.nontech, storage.multer.single("resume"), storage.uploadToGCP, controller.marketer);
-
-// router.post("/applications/swift", validation.swift, storage, controller.swift);
+router.post("/applications/marketer", validation.nontech, controller.marketer);
 
 module.exports = router;
