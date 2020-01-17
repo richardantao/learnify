ab2b8dc9805e8991e0087844248b610c4a889a49
Richard Antao <richardmantao@gmail.com>
2020-01-16 22:53:54 -0500

    Converted to functional component

--- views/src/components/beta/global/Nav/Nav.js
+++ views/src/components/beta/global/Nav/Nav.js
@@ -1,64 +1,44 @@
-import React, { Component, Fragment } from "react";
-
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faTachometerAlt, faCalendarAlt, faTasks, faGraduationCap,
 	faUniversity, faSearch, faCog, faQuestionCircle
 } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "react-bootstrap";
+import { Button } from "reactstrap";
 import Logo from "../Logo";
 
-import "./Nav.scss";
+import "../Nav.scss";
 
-class Nav extends Component {
-	state = {
-
-	};
-	
-	static propTypes = {
-
-	};
-	
-	render() {
-		return ( 
-			<Fragment>
-				<Logo/> 
-				<nav className="beta-nav" role="navigation">
-					<Button href="/beta/dashboard">
-						<FontAwesomeIcon icon={faTachometerAlt}/>
-					</Button>
-					<Button href="/beta/calendar">
-						<FontAwesomeIcon icon={faCalendarAlt}/>
-					</Button>
-					<Button href="/beta/academics">
-						<FontAwesomeIcon icon={faUniversity}/>
-					</Button>
-					<Button href="/beta/planner">
-						<FontAwesomeIcon icon={faTasks}/>
-					</Button>
-					{/* <Button href="/search">
-						<FontAwesomeIcon icon={faSearch}/>
-					</Button> */}
-					<Button href="/beta/settings" className="secondary-nav-button">
-						<FontAwesomeIcon icon={faCog}/>
-					</Button>
-					<Button href="/beta/help" className="secondary-nav-button">
-						<FontAwesomeIcon icon={faQuestionCircle}/>
-					</Button>
-				</nav>
-			</Fragment>
-		);
-	};
+const Nav = () => {
+	return ( 
+		<>
+			<Logo/> 
+			<nav className="beta-nav" role="navigation">
+				<Button href="/beta/dashboard">
+					<FontAwesomeIcon icon={faTachometerAlt}/>
+				</Button>
+				<Button href="/beta/calendar">
+					<FontAwesomeIcon icon={faCalendarAlt}/>
+				</Button>
+				<Button href="/beta/academics">
+					<FontAwesomeIcon icon={faUniversity}/>
+				</Button>
+				<Button href="/beta/planner">
+					<FontAwesomeIcon icon={faTasks}/>
+				</Button>
+				{/* <Button href="/search">
+					<FontAwesomeIcon icon={faSearch}/>
+				</Button> */}
+				<Button href="/beta/settings" className="secondary-nav-button">
+					<FontAwesomeIcon icon={faCog}/>
+				</Button>
+				<Button href="/beta/help" className="secondary-nav-button">
+					<FontAwesomeIcon icon={faQuestionCircle}/>
+				</Button>
+			</nav>
+		</>
+	);
 };
 
-const mapStateToProps = state => ({
-	// isAuthenticated: state.auth.isAuthenticated,
-	error: state.error
-});
-
-const mapDispatchToProps = { };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Nav);
+export default Nav;
