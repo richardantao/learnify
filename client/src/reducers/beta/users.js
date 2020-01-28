3ff5c886bcd9f21d7204010834ee37384f3d5025
Richard Antao <richardmantao@gmail.com>
2020-01-28 13:47:27 -0500

    Initialized reducer

---
+++ client/src/reducers/beta/users.js
@@ -0,0 +1,67 @@
+import { 
+    LOADING_SETTINGS, 
+    EDIT_PROFILE, UPDATE_PROFILE, DELETE_PROFILE,
+    EDIT_PASSWORD, UPDATE_PASSWORD,
+    EDIT_PREFERENCES, UPDATE_PREFERENCES
+} from "../../actions/types";
+
+const initialState = {
+    loading: false,
+    profile: {},
+    password: {},
+    preferences: {}
+};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case LOADING_SETTINGS: 
+            return {
+                ...state,
+                loading: true
+            };
+        case EDIT_PROFILE:
+            return {
+                ...state,
+                loading: false,
+                profile: {}
+            };
+        case UPDATE_PROFILE:
+            return {
+                ...state,
+                loading: false,
+                profile: {}
+            };
+        case DELETE_PROFILE:
+            return {
+                ...state,
+                loading: false,
+                profile: {}
+            };
+        case EDIT_PASSWORD:
+            return {
+                ...state,
+                loading: false,
+                password: {}
+            };
+        case UPDATE_PASSWORD: 
+            return {
+                ...state,
+                loading: false,
+                password: {}
+            };
+        case EDIT_PREFERENCES: 
+            return {
+                ...state,
+                loading: false,
+                preferences: {}
+            };
+        case UPDATE_PREFERENCES:
+            return {
+                ...state,
+                loading: false,
+                preferences: {}
+            };
+        default: 
+            return state;
+    };
+};
