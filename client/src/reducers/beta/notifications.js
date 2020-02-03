d6a6724358252314029b5f3be4c46c5869a41323
Richard Antao <richardmantao@gmail.com>
2020-02-02 20:20:58 -0500

    Initialized reducer

---
+++ client/src/reducers/beta/notifications.js
@@ -0,0 +1,20 @@
+import { } from "../../actions/types";
+
+const initialState = {
+    loading: false,
+};
+
+export default (state = initialState, action) => {
+    switch(action.type) {
+        case test:
+            return {
+
+            };
+        case bar: 
+            return {
+
+            };
+        default: 
+            return state;
+    };
+};
