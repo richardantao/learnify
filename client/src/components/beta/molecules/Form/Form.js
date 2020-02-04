e35fd59c43c30d0ac8cfc36e1c0157a9d6f322da
Richard Antao <richardmantao@gmail.com>
2020-02-04 18:30:26 -0500

    Migrated to molecules

---
+++ client/src/components/beta/molecules/Form/Form.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form as Frm} from "reactstrap";
+
+const Form = props => {
+    return (
+        <Frm onSubmit={props.handleSubmit}>
+            
+        </Frm>
+    );
+};
+
+export default Form;
