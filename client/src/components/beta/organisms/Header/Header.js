b83254fd94514fcce7fe788187fe864331229a52
Richard Antao <richardmantao@gmail.com>
2020-02-05 17:44:41 -0500

    Migrated file

--- client/src/components/beta/organisms/Header/Header.js
+++ client/src/components/beta/organisms/Header/Header.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-const Header = props => {
-    return (
-        <div></div>
-    );
-};
+const Header = props => { return <h1>{props.header}</h1> }
 
 export default Header;
+
