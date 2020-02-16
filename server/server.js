f861ab874f7eaede4c4e99715dc3f7ad9b461a67
Richard Antao <richardmantao@gmail.com>
2020-02-16 01:00:28 -0500

    Removed redis configuration

--- server/server.js
+++ server/server.js
@@ -14,60 +14,56 @@ const port = process.env.PORT;
 const env = process.env.NODE_ENV || "development";
 // const corsOptions = ["https://learnify.ca", "https://www.learnify.ca", "http://localhost"];
 require("./config/db");
-require("./config/cache");
 
 /* --- Middleware --- */
+app.use(helmet());
+app.use(cors(/*{ 
+	origin: (origin, callback) => {
+		if(corsOptions.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			console.log("Not allowed by CORS");
+			callback(new Error("Not allowed by CORS"));
+		};
+	}
+}*/));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-if (env === "production") {
-	app.use(helmet());
-	app.use(cors(/*{ 
-		origin: (origin, callback) => {
-			if(corsOptions.indexOf(origin) !== -1) {
-				callback(null, true);
-			} else {
-				console.log("Not allowed by CORS");
-				callback(new Error("Not allowed by CORS"));
-			};
-		}
-	}*/));
-	app.use(express.static(path.join(__dirname, "../client/build")));
-	app.use(compression());
-	logger((tokens, req, res) => {
-		return [
-		  tokens.method(req, res),
-		  tokens.url(req, res),
-		  tokens.status(req, res),
-		  tokens.res(req, res, "content-length"), "-",
-		  tokens["response-time"](req, res), "ms"
-		].join(" ")
-	});
-} else {
-	app.use(logger("dev"));
-	app.use(cors());
-};
+app.use(logger("dev"));
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(compression());
+logger((tokens, req, res) => {
+	return [
+		tokens.method(req, res),
+		tokens.url(req, res),
+		tokens.status(req, res),
+		tokens.res(req, res, "content-length"), "-",
+		tokens["response-time"](req, res), "ms"
+	].join(" ")
+});
 
 /* --- Routes --- */
 /* Beta  */
-app.use("/api/v1/", require("./api/routes/auth"));
+app.use("/api/v1", require("./api/routes/auth"));
 app.use("/api/v1/cron", require("./api/routes/cron"));
-app.use("/api/v1/", require("./api/routes/users"));
+app.use("/api/v1", require("./api/routes/users"));
 
-app.use("/api/v1/", require("./api/routes/years"));
-app.use("/api/v1/", require("./api/routes/terms"));
-app.use("/api/v1/", require("./api/routes/courses"));
-app.use("/api/v1/", require("./api/routes/classes"));
-app.use("/api/v1/", require("./api/routes/assessments"));
-app.use("/api/v1/", require("./api/routes/tasks"));
+app.use("/api/v1/years", require("./api/routes/years"));
+app.use("/api/v1", require("./api/routes/terms"));
+app.use("/api/v1", require("./api/routes/courses"));
+app.use("/api/v1", require("./api/routes/classes"));
+app.use("/api/v1", require("./api/routes/assessments"));
+app.use("/api/v1", require("./api/routes/tasks"));
+app.use("/api/v1/bugs", require("./api/routes/bugs"));
+app.use("/api/v1/feedback", require("./api/routes/feedback"));
 
 app.use("/api/v1/", require("./api/routes/search"));
 
 /* Root */
-app.use("/", require("./api/routes/root"));
+app.use("", require("./api/routes/root"));
 
 /* Team */
-app.use("/", require("./api/routes/team"));
+app.use("", require("./api/routes/team"));
 
 /* Fetch Client Side Rendering */
 app.get("*", (req, res) => {
