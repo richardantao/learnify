82386ff5898264bcd086dffda9c3e75d45a4dc2e
Richard Antao <richardmantao@gmail.com>
2020-02-17 23:47:34 -0500

    Segmented meta data

--- server/api/models/Bugs.js
+++ server/api/models/Bugs.js
@@ -9,7 +9,9 @@ module.exports = model("bugs", new Schema({
     where: { type: String, required: true },
     type: { type: String, required: true },
     message: { type: String, required: true },
-    createdAt: { type: Date, default: () => moment().utc(moment.utc().format()).local().format("YYYY MM DD, hh:mm") },
-    updatedAt: { type: Date, default: () => moment().utc(moment.utc().format()).local().format("YYYY MM DD, hh:mm") },
+    meta: {
+        createdAt: { type: Date, default: () => moment().utc(moment.utc().format()).local().format("YYYY MM DD, hh:mm") },
+        updatedAt: { type: Date, default: () => moment().utc(moment.utc().format()).local().format("YYYY MM DD, hh:mm") }
+    },
     resolved: { type: Boolean, default: false }
 }));
