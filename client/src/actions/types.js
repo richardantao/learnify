b478a5c9da3993837761370cf14fbd1807801b16
Richard Antao <richardmantao@gmail.com>
2020-02-23 13:48:55 -0500

    Updated types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,4 @@
-/* AUTH */
+/* --- AUTH --- */
 export const USER_LOADING = "USER_LOADING";
 export const USER_LOADED = "USER_LOADED";
 
@@ -17,66 +17,69 @@ export const REGISTER_FAILED = "REGISTER_FAILED";
 export const GET_ERRORS = "GET_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
-/* DATA */
+/* --- DATA --- */
 // years
 export const LOADING_YEARS = "LOADING_YEARS";
+export const CREATE_YEAR = "CREATE_YEAR";
 export const FETCH_YEARS = "FETCH_YEARS";
 export const EDIT_YEAR = "EDIT_YEAR";
-export const NEW_YEAR = "NEW_YEAR";
-export const CREATE_YEAR = "CREATE_YEAR";
 export const UPDATE_YEAR = "UPDATE_YEAR";
 export const DELETE_YEAR = "DELETE_YEAR";
 
 // terms
 export const LOADING_TERMS = "LOADING_TERMS";
-export const FETCH_TERMS = "FETCH_TERMS";
-export const EDIT_TERM = "EDIT_TERM";
 export const NEW_TERM = "NEW_TERM";
 export const CREATE_TERM = "CREATE_TERM";
+export const FETCH_TERMS = "FETCH_TERMS";
+export const EDIT_TERM = "EDIT_TERM";
 export const UPDATE_TERM = "UPDATE_TERM";
 export const DELETE_TERM = "DELETE_TERM";
 
 // courses
 export const LOADING_COURSES = "LOADING_COURSES";
-export const FETCH_COURSES = "FETCH_COURSES";
-export const EDIT_COURSE = "EDIT_COURSE";
 export const NEW_COURSE = "NEW_COURSE";
 export const CREATE_COURSE = "CREATE_COURSE";
+export const FETCH_COURSES = "FETCH_COURSES";
+export const EDIT_COURSE = "EDIT_COURSE";
 export const UPDATE_COURSE = "UPDATE_COURSE";
 export const DELETE_COURSE = "DELETE_COURSE";
 
 // classes
 export const LOADING_CLASSES = "LOADING_CLASSES";
-export const FETCH_CLASSES = "FETCH_CLASSES";
-export const EDIT_CLASS = "EDIT_CLASS";
 export const NEW_CLASS = "NEW_CLASS";
 export const CREATE_CLASS = "CREATE_CLASS";
+export const FETCH_CLASSES_FOR_DASH = "FETCH_CLASSES_FOR_DASH";
+export const FETCH_CLASSES_BY_TERM = "FETCH_CLASSES_BY_TERM";
+export const FETCH_CLASSES_BY_COURSE = "FETCH_CLASSES_BY_COURSE";
+export const EDIT_CLASS = "EDIT_CLASS";
 export const UPDATE_CLASS = "UPDATE_CLASS";
 export const DELETE_CLASS = "DELETE_CLASS";
 
 // assessments
 export const LOADING_ASSESSMENTS = "LOADING_ASSESSMENTS";
+export const NEW_ASSESSMENT = "NEW_ASSESSMENT";
+export const CREATE_ASSESSMENT = "CREATE_ASSESSMENT";
 export const FETCH_ASSESSMENTS = "FETCH_ASSESSMENTS";
 export const FETCH_PAST_ASSESSMENTS = "FETCH_PAST_ASSESSMENTS";
 export const EDIT_ASSESSMENT = "EDIT_ASSESSMENT";
-export const NEW_ASSESSMENT = "NEW_ASSESSMENT";
-export const CREATE_ASSESSMENT = "CREATE_ASSESSMENT";
 export const UPDATE_ASSESSMENT = "UPDATE_ASSESSMENT";
 export const DELETE_ASSESSMENT = "DELETE_ASSESSMENT";
 
 // tasks
 export const LOADING_TASKS = "LOADING_TERMS";
+export const NEW_TASK = "NEW_TASK";
+export const CREATE_TASK = "CREATE_TASK";
 export const FETCH_TASKS = "FETCH_TASKS";
 export const FETCH_PAST_TASKS = "FETCH_PAST_TASKS";
 export const EDIT_TASK = "EDIT_TASK";
-export const NEW_TASK = "NEW_TASK";
-export const CREATE_TASK = "CREATE_TASK";
 export const UPDATE_TASK = "UPDATE_TASK";
 export const DELETE_TASK = "DELETE_TASK";
 
-// settings
+// user
+export const LOADING_SETTINGS = "LOADING_SETTINGS";
 export const EDIT_PROFILE = "EDIT_PROFILE";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
+export const DELETE_PROFILE = "DELETE_PROFILE";
 
 export const EDIT_PASSWORD = "EDIT_PASSWORD";
 export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
@@ -84,6 +87,7 @@ export const UPDATE_PASSWORD = "UPDATE_PASSWORD";
 export const EDIT_PREFERENCES = "EDIT_PREFERENCES";
 export const UPDATE_PREFERENCES = "UPDATE_PREFERENCES";
 
+// bugs
 export const LOADING_BUGS = "LOADING_BUGS";
 export const CREATE_BUG = "CREATE_BUG";
 export const FETCH_BUGS = "FETCH_BUGS";
@@ -91,36 +95,13 @@ export const EDIT_BUG = "EDIT_BUG";
 export const UPDATE_BUG = "UPDATE_BUG";
 export const DELETE_BUG = "DELETE_BUG";
 
+// feedback
 export const LOADING_FEEDBACK = "LOADING_FEEDBACK";
 export const CREATE_FEEDBACK = "CREATE_FEEDBACK";
 export const FETCH_FEEDBACK = "FETCH_FEEDBACK";
 export const EDIT_FEEDBACK = "EDIT_FEEDBACK";
 export const UPDATE_FEEDBACK = "UPDATE_FEEDBACK";
 export const DELETE_FEEDBACK = "DELETE_FEEDBACK";
-
-/* FUNCTIONS */
-
-// counter in dashboard
-export const COUNT_DATA = "COUNT_DATA";
-
-/* UI */
-
-// nav
-export const LOADING_VIEW = "LOADING_VIEW"
-export const RENDER_DASHBOARD = "RENDER_DASHBOARD";
-export const RENDER_CALENDAR = "RENDER_CALENDAR";
-export const RENDER_ACADEMICS = "RENDER_ACADEMICS";
-export const RENDER_PLANNER = "RENDER_PLANNER";
-export const RENDER_SEARCH = "RENDER_SEARCH";
-export const RENDER_SETTINGS = "RENDER_SETTINGS";
-export const RENDER_HELP = "RENDER_HELP";
-
-// settings
-export const LOADING_SETTINGS = "LOADING_SETTINGS";
-export const RENDER_PROFILE = "RENDER_PROFILE";
-export const RENDER_PASSWORD = "RENDER_PASSWORD";
-export const RENDER_PREFERENCES = "RENDER_PREFERENCES";
-export const RENDER_INTEGRATIONS = "RENDER_INTEGRATIONS";
 
 /* --- ROOT --- */
 export const PROCESSING_FORM = "PROCESSING_FORM";
