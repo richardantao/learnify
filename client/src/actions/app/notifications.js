8d8a7e178478e2143b079ca3bf5682e1137632a0
Richard Antao <richardmantao@gmail.com>
2020-03-02 13:15:59 -0500

    Updated notifications state management

--- client/src/actions/app/notifications.js
+++ client/src/actions/app/notifications.js
@@ -1,10 +1,66 @@
 import {
-    NOTIFICATIONS_REQUESTED
+    NOTIFICATIONS_REQUESTED, 
+    NOTIFICATION_CREATED,
+    NOTIFATIONS_FETCHED,
+    NOTIFICATION_TOGGLED, NOTIFICATION_DELETED
 } from "../types";
+import { returnErrors } from "../auth/errors";
 import axios from "axios";
 
 export const setLoading = () => {
     return {
         type: NOTIFICATIONS_REQUESTED
     };
+};
+
+export const initiailizeNotification = notification => (dispatch, getState) => {
+    dispatch(setLoading());
+
+    axios.post("/api/v1/notifcations", notification, tokenConfig(getState))
+    .then(res => dispatch({
+        type: NOTIFICATION_CREATED,
+        payload: res.data
+    }))
+    .catch(err => {
+        returnErrors(err.res.data, err.res.status, "NOTIFCATIONS_ERROR");
+    });
+};
+
+export const fetchNotifications = () => (dispatch, getState) => {
+    dispatch(setLoading());
+    
+    axios.get("/api/v1/notifcations", tokenConfig(getState))
+    .then(res => dispatch({
+        type: NOTIFATIONS_FETCHED,
+        payload: res.data
+    }))
+    .catch(err => {
+        returnErrors(err.res.data, err.res.status, "NOTIFCATIONS_ERROR");
+    });
+};
+
+export const toggleNotification = (id, notification) => (dispatch, getState) => {
+    dispatch(setLoading());
+
+    axios.put(`/api/v1/notifcations/${id}`, notification, tokenConfig(getState))
+    .then(res => dispatch({
+        type: NOTIFICATION_TOGGLED,
+        payload: res.data
+    }))
+    .catch(err => {
+        returnErrors(err.res.data, err.res.status, "NOTIFCATIONS_ERROR");
+    });
+};
+
+export const deleteNotification = id => (dispatch, getState) => {
+    dispatch(setLoading());
+
+    axios.delete(`/api/v1/notifications/${id}`, tokenConfig(getState))
+    .then(res => dispatch({
+        type: NOTIFICATION_DELETED,
+        payload: id
+    }))
+    .catch(err => {
+        returnErrors(err.res.data, err.res.status, "NOTIFCATIONS_ERROR");
+    });
 };
--- client/src/reducers/app/notifications.js
+++ client/src/reducers/app/notifications.js
@@ -1,24 +1,46 @@
 import { 
-    NOTIFICATIONS_REQUESTED, NOTIFICATIONS_ERROR
+    NOTIFICATIONS_REQUESTED, NOTIFICATIONS_ERROR,
+    NOTIFICATION_CREATED, 
+    NOTIFATIONS_FETCHED,
+    NOTIFICATION_TOGGLED, NOTIFICATION_DELETED
 } from "../../actions/types";
 
 const initialState = {
     loading: false,
+    notifications: []
 };
 
 export default (state = initialState, action) => {
     switch(action.type) {
         case NOTIFICATIONS_REQUESTED:
             return {
+                ...state,
                 loading: true
             };
         case NOTIFICATIONS_ERROR:
             return {
+                ...state,
                 loading: false
             };
-        case bar: 
+        case NOTIFICATION_CREATED: 
             return {
-
+                ...state,
+                loading: false,
+                notifications: [...state.notifications, action.payload]
+            };
+        case NOTIFICATION_TOGGLED:
+            return {
+                ...state,
+                loading: false,
+                notifications: state.notifications.map(notification => {
+                    const { _id, } = action.payload;
+                })
+            };
+        case NOTIFICATION_DELETED:
+            return {
+                ...state,
+                loading: false,
+                notifications: state.notifications.filter(notification => notification._id !== action.payload )
             };
         default: 
             return state;
