2ca7d32d90257afb639702a0b800c80cb9c0d8ef
Richard Antao <richardmantao@gmail.com>
2020-03-15 14:39:08 -0400

    Changed message property

--- server/api/models/Feedback.js
+++ server/api/models/Feedback.js
@@ -7,7 +7,7 @@ module.exports = model("feedback", new Schema({
     _id: Schema.Types.ObjectId,
     user: { type: Schema.Types.ObjectId, ref: "users" },
     subject: { type: String, required: true },
-    message: { type: String, required: true },
+    description: { type: String, required: true },
     date: { type: Date, default: () => moment().utc(moment.utc().format()).local().format("YYYY MM DD, hh:mm") },
 }, { 
     versionKey: false 
