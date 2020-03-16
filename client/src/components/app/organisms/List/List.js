2be0f5bf7f7d093f9e98f74368f7057480ae30a1
Richard Antao <richardmantao@gmail.com>
2020-03-16 15:58:22 -0400

    Imported child molecules

--- client/src/components/app/organisms/List/List.js
+++ client/src/components/app/organisms/List/List.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Col } from "reactstrap";
+
+/* -- Atoms -- */
 import Empty from "../../atoms/Empty";
 
-export default ({ id, className, data, empty }) => {
+/* -- Organisms -- */
+import ListHeader from "../../molecules/ListHeader";
+import ListBody from "../../molecules/ListBody";
+
+import "./List.scss";
+
+export default ({ id, className, header, action, data, empty }) => {
     return (
-        <>
-            { data ? (
-                <Col id={id} className={className}>
-                    {data}
-                </Col>   
-            ): <Empty empty={empty}/>}
-        </>
+        <Col id={id} className={className}>
+            <ListHeader
+                header={header}
+                action={action}
+            />
+            <hr/>
+            <ListBody
+                data={ data ? data : <Empty empty={empty}/>}
+            />
+        </Col>
     );
 };
