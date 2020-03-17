34fb10bda8f3cf54fad4e405b3fb55434060977a
Richard Antao <richardmantao@gmail.com>
2020-03-17 16:16:50 -0400

    Refactored code

---
+++ client/src/team/Team.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import {
+    Switch,
+    Route,
+    withRouter
+} from "react-router-dom";
+import { connect } from "react-redux";
+
+import Dashboard from "./components/pages/Dashboard";
+import KPI from "./components/pages/KPI";
+import NotFound from "./components/pages/NotFound";
+
+import "./Team.scss";
+
+class Team extends Component {
+    static propTypes = {
+
+    };
+
+    componentDidUpdate(prevProps) {
+        const { } =this.props; 
+    };
+
+    render() {
+
+            
+        return (
+            <Switch>
+                <Route path="/team/dashboard" component={Dashboard}/>
+                <Route path="/team/kpi" component={KPI}/>
+                <Route path="*" component={NotFound}/>
+            </Switch>
+        );
+    };
+};
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = {};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Team));
