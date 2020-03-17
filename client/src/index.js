fa702c065ed616b95949741c12fdb2c2047012e1
Richard Antao <richardmantao@gmail.com>
2020-03-16 21:23:46 -0400

    Imported Switch groups

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import * as serviceWorker from "./serviceWorker";
+
+import App from "./app/App";
+import Public from "./public/Public";
+import Team from "./team/Team";
+
+import "bootstrap/dist/css/bootstrap.css";
 
 const customHistory = createBrowserHistory();
 
 render (
 	<Router history={customHistory}>
+		<Provider store={store}>
+			<App/>
+			<Public/>
+			<Team/>
+		</Provider>
 		<App/>
 	</Router>,
 	document.querySelector("#learnify")
 );
 
-// register service worker
 serviceWorker.register();
