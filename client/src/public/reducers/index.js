f0220518a6ac4de47d3b888a586b091e9a607f9e
Richard Antao <richardmantao@gmail.com>
2020-03-18 14:41:52 -0400

    Merged reducers into single store

--- client/src/public/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-
-import error from "./errors";
-import form from "./form";
-
-export default combineReducers({ error, form });
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,28 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import appReducer from "./app/reducers";
-import publicReducer from "./public/reducers";
-import teamReducer from "./team/reducers";
+
+/* --- Reducers --- */
+// Auth
+// import auth from "./app/reducers/auth/auth";
+import error from "./app/reducers/auth/errors";
+
+// Data
+import user from "./app/reducers/data/users";
+import year from "./app/reducers/data/years";
+import term from "./app/reducers/data/terms";
+import course from "./app/reducers/data/courses";
+import classes from "./app/reducers/data/classes";
+import assessment from "./app/reducers/data/assessments";
+import task from "./app/reducers/data/tasks";
+import bug from "./app/reducers/data/bugs";
+import feedback from "./app/reducers/data/feedback";
+
+// Interface
+import meta from "./app/reducers/interface/meta";
+import property from "./app/reducers/interface/properties";
+
+// Public Form
+import form from "./public/reducers/form";
 
 const initialState = {};
 
@@ -10,7 +30,22 @@ const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = () => ({ appReducer, publicReducer, teamReducer });
+const rootReducer = combineReducers({
+    // auth,
+    meta,
+    error,
+    property,
+    user,
+    year,
+    term,
+    course,
+    classes,
+    assessment,
+    task,
+    bug,
+    feedback,
+    form
+});
 
 const store = createStore(
     rootReducer,
