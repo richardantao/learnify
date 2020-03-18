dfcfd6f19392d5288e3037678c6174ddc86607a1
Richard Antao <richardmantao@gmail.com>
2020-03-18 02:02:04 -0400

    Added preferences

--- client/src/app/actions/interface/meta.js
+++ client/src/app/actions/interface/meta.js
@@ -1,4 +1,11 @@
-import { ACTIVE_TERM_REQUESTED, ACTIVE_TERM_SET } from "../types";
+import { 
+    ACTIVE_TERM_REQUESTED, 
+    ACTIVE_TERM_SET, 
+    DEFAULT_DURATION_FETCHED,
+    DEFAULT_CALENDAR_FETCHED, 
+    START_TIME_FETCHED,
+    START_DAY_FETCHED
+} from "../types";
 import { tokenConfig } from "../auth/auth";
 import { returnErrors } from "../auth/errors";
 import axios from "axios";
@@ -25,5 +32,57 @@ export const setActiveTerm = () => (dispatch, getState) => {
     }))
     .catch(err => dispatch(
         returnErrors(err.res.data, err.res.status, "ACTIVE_TERM_FAILED")
+    ));
+};
+
+export const getStartTime = () => (dispatch, getState) => {
+    dispatch(setLoading());
+    
+    axios.get("/api/v1/users/preferences/?time=fetch"/*, tokenConfig(getState)*/)
+    .then(res => dispatch({
+        type: START_TIME_FETCHED,
+        payload: res.data
+    }))
+    .catch(err => dispatch(
+        returnErrors(err.response.data, err.response.status, "")
+    ));
+};
+
+export const getStartDay = () => (dispatch, getState) => {
+    dispatch(setLoading());
+    
+    axios.get("/api/v1/users/preferences/?day=fetch"/*, tokenConfig(getState)*/)
+    .then(res => dispatch({
+        type: START_DAY_FETCHED,
+        payload: res.data
+    }))
+    .catch(err => dispatch(
+        returnErrors(err.response.data, err.response.status, "")
+    ));
+};
+
+export const getDefaultDuration = () => (dispatch, getState) => {
+    dispatch(setLoading());
+    
+    axios.get("/api/v1/users/preferences/?duration=fetch"/*, tokenConfig(getState)*/)
+    .then(res => dispatch({
+        type: DEFAULT_DURATION_FETCHED,
+        payload: res.data
+    }))
+    .catch(err => dispatch(
+        returnErrors(err.response.data, err.response.status, "")
+    ));
+};
+
+export const getDefaultCalendar = () => (dispatch, getState) => {
+    dispatch(setLoading());
+    
+    axios.get("/api/v1/users/preferences/?calendar=fetch"/*, tokenConfig(getState)*/)
+    .then(res => dispatch({
+        type: DEFAULT_CALENDAR_FETCHED,
+        payload: res.data
+    }))
+    .catch(err => dispatch(
+        returnErrors(err.response.data, err.response.status, "")
     ));
 };
--- client/src/app/reducers/interface/meta.js
+++ client/src/app/reducers/interface/meta.js
@@ -1,8 +1,15 @@
-import { ACTIVE_TERM_REQUESTED, ACTIVE_TERM_FAILED, ACTIVE_TERM_SET } from "../../actions/types";
+import { 
+    ACTIVE_TERM_REQUESTED, ACTIVE_TERM_FAILED, ACTIVE_TERM_SET,
+    START_TIME_FETCHED, START_DAY_FETCHED, DEFAULT_DURATION_FETCHED, DEFAULT_CALENDAR_FETCHED
+} from "../../actions/types";
 
 const initialState = {
     loading: false,
-    activeTerm: null
+    activeTerm: null,
+    startTime: null,
+    startDay: null,
+    defaultDuration: null,
+    defaultCalendar: null,
 };
 
 export default (state = initialState, action) => {
@@ -22,6 +29,30 @@ export default (state = initialState, action) => {
                 ...state,
                 loading: false,
                 activeTerm: action.payload 
+            };
+        case START_TIME_FETCHED:
+            return {
+                ...state,
+                loading: false,
+                startTime: action.payload 
+            };
+        case START_DAY_FETCHED:
+            return {
+                ...state,
+                loading: false,
+                startDay: action.payload 
+            };
+        case DEFAULT_DURATION_FETCHED:
+            return {
+                ...state,
+                loading: false,
+                defaultDuration: action.payload 
+            };
+        case DEFAULT_CALENDAR_FETCHED: 
+            return {
+                ...state,
+                loading: false,
+                defaultCalendar: action.payload 
             };
         default: 
             return state;
