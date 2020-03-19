6e834ab138575eb1bf6bea4d64aa33ae1074072f
Richard Antao <richardmantao@gmail.com>
2020-03-18 20:47:11 -0400

    Converted to class component

--- client/src/app/components/atoms/Switch/Switch.js
+++ client/src/app/components/atoms/Switch/Switch.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { ButtonGroup, Button } from "reactstrap";
 
-export default ({ primaryRef, primaryText, secondaryRef, secondaryText, onClick }) => {
-    return (
-        <ButtonGroup>
-            <Button href={primaryRef} onClick={onClick}>
-                {primaryText}
-            </Button>
-            <Button href={secondaryRef} onClick={onClick}>
-                {secondaryText}
-            </Button>
-        </ButtonGroup>
-    );
+export default class Switch extends Component {
+    state = {
+        past: false
+    };
+
+    componentDidUpdate(prevProps) {
+        const { past } = this.props;
+
+        if(past !== prevProps.past) {
+            this.setState({ past });
+        };
+    };
+
+    render() {
+        const { primaryRef, primaryText, secondaryRef, secondaryText, past } = this.props;
+        
+        return (
+            <ButtonGroup>
+                { past ? 
+                    <>
+                        <Button href={primaryRef}>
+                            {primaryText}
+                        </Button>
+                        <Button href={secondaryRef} className="active-switch">
+                            {secondaryText}
+                        </Button>
+                    </>
+                    : 
+                    <>
+                        <Button href={primaryRef}  className="active-switch">
+                            {primaryText}
+                        </Button>
+                        <Button href={secondaryRef}>
+                            {secondaryText}
+                        </Button>
+                    </>
+                }
+            </ButtonGroup>
+        );
+    };
 };
