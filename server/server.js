ad58d6d65faaf6453d9376a20135ac0c1d143778
Richard Antao <richardmantao@gmail.com>
2020-04-01 00:29:00 -0400

    Added payload limit

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(cors(/*{
 		};
 	}
 }*/));
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "../client/build")));
