d9c574d27d30ab5d25a93d460fa38832546a43d5
Richard Antao <richardmantao@gmail.com>
2020-04-03 18:49:13 -0400

    Added conditional rendering

--- client/src/app/components/organisms/List/List.js
+++ client/src/app/components/organisms/List/List.js
@@ -10,17 +10,25 @@ import ListBody from "../../molecules/ListBody";
 
 import "./List.scss";
 
-export default ({ id, className, header, action, data, empty }) => {
+export default ({ id, className, header, action, data, empty, needHeader }) => {
     return (
         <Col id={id} className={className}>
-            <ListHeader
-                header={header}
-                action={action}
-            />
-            <hr/>
-            <ListBody
-                data={ data ? data : <Empty empty={empty}/>}
-            />
+            { needHeader ? (
+                <>
+                    <ListHeader
+                    header={header}
+                    action={action}
+                    />
+                    <hr/>
+                    <ListBody
+                        data={ data ? data : <Empty empty={empty}/>}
+                    />
+                </>
+            ): (
+                <ListBody
+                    data={ data ? data : <Empty empty={empty}/>}
+                />
+            )}            
         </Col>
     );
 };
