b4ff19e4d5242a07a73c714ab9c1e49ce221a6bc
Richard Antao <richardmantao@gmail.com>
2020-04-04 01:44:21 -0400

    Migrated UI templates

--- client/src/app/components/pages/Dashboard/Dashboard.js
+++ client/src/app/components/pages/Dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-
-import moment from "moment";
+import { isMobile, isTablet } from "react-device-detect"; 
 
 /* Redux Operations */
 import { connect } from "react-redux";
@@ -17,21 +16,11 @@ import {
 } from "../../../actions/data/assessments";
 import PropTypes from "prop-types";
 
-import { Row, Col, Button, Input } from "reactstrap";
+import { Row } from "reactstrap";
 
 import Loadable from "react-loadable";
 
-/* --- Atoms --- */
-import Today from "../../atoms/Today";
-
-/* --- Molecules --- */
-
-
-/* --- Organisms --- */
-import DashboardHeader from "../../organisms/DashboardHeader";
-import List from "../../organisms/List";
-
-import "./Dashboard.scss";
+import "./Dashboard.scss"; // distribute to templates
 
 class Dashboard extends Component {
 	state = {
@@ -39,7 +28,6 @@ class Dashboard extends Component {
 	};
 
 	static propTypes = {
-		// isAuthenticated: PropTypes.bool,
 		meta: PropTypes.object,
 		error: PropTypes.object,
 		classes: PropTypes.object,
@@ -52,7 +40,6 @@ class Dashboard extends Component {
 		deleteClass: PropTypes.func.isRequired,
 		fetchTasks: PropTypes.func.isRequired,
 		editTask: PropTypes.func.isRequired,
-		updateTask: PropTypes.func.isRequired,
 		deleteTask: PropTypes.func.isRequired,
 		fetchAssessments: PropTypes.func.isRequired,
 		editAssessment: PropTypes.func.isRequired,
@@ -92,6 +79,8 @@ class Dashboard extends Component {
 
 	render() {
 		const { 
+			meta,
+			error,
 			classes: { classes },
 			task: { tasks },
 			assessment: { assessments },
@@ -108,147 +97,80 @@ class Dashboard extends Component {
 					<title>My Learnify | Dashboard</title>
 				</Helmet>
 				<Row id="dashboard">
-					<Col>
-						<Row>
-							<DashboardHeader className="dashboard-header" heading="Today" extra={<Today/>} type="Classes" count={classes.length}/>
-							<DashboardHeader className="dashboard-header" heading="Tasks" extra={<TaskNew/>} type="Tasks" count={tasks.length}/>
-							<DashboardHeader className="dashboard-header" heading="Assessments" extra={null} type="Assessments" count={assessments.length}/>
-						</Row>
-						<Row id="dashboard-columns" className="body">
-							<List 
-								id="classes" 
-                                className="classes-list"
-								data={classes.map(({ _id, title, course, location, date: { start, end } }) => {
-									return (
-										<Row key={_id}>
-											<Col>
-												<h4>{title}</h4>
-												<h5>{course.title}</h5>
-											</Col>
-											<Col>
-												<p>{location}</p>
-												<p>
-													{ moment(start).startOf("day") - moment(start) < 60*60*12 && moment(end).startOf("day") - moment(end) > 60*60*12 ? (
-														`${moment(start, "h:mm a")} - ${moment(end, "h:mm a")}`
-													): `${moment(start, "h:mm")} - ${moment(end, "h:mm a")}` }
-												</p>
-											</Col>
-											<Col>
-												<ClassEdit onClick={editClass(_id)}/>
-												<Button onClick={deleteClass(_id)}>
-													icon
-												</Button>
-											</Col>
-										</Row>
-									);
-								})}
-							/>
-							<List 
-								id="tasks" 
-                                className="tasks-list"
-								data={tasks.map(({ _id, title, course, type, deadline, completed }) => {
-									return (
-										<Row key={_id}>
-											<Col xs="1" sm="1" md="1" lg="1" xl="1">
-												<Input	
-													name="completed"
-													type="checkbox"
-													checked={completed}
-													onChange={toggleTaskCompletion(_id)}
-												/>
-											</Col>
-											<Col xs="5" sm="5" md="5" lg="5" xl="5">
-												<h4>{title}</h4>
-												<h5>{course.title}</h5>
-											</Col>
-											<Col xs="4" sm="4" md="4" lg="4" xl="4">
-												<p>{type}</p>
-												<p>{moment(deadline, "MMMM Do, h:mm a")}</p>
-											</Col>
-											<Col xs="2" sm="2" md="2" lg="2" xl="2">
-												<TaskEdit onClick={editTask(_id)}/>
-												<Button onClick={deleteTask(_id)}>
-													icon
-												</Button>
-											</Col>
-										</Row>
-									);
-								})}
-							/>
-							<List 
-								id="assessments" 
-                                className="assessments-list"
-								data={assessments.map(({ _id, title, course, type, date: { start, end }, completed }) => {
-									return (
-										<Row key={_id}>
-											<Col xs="1" sm="1" md="1" lg="1" xl="1">
-												<Input
-													name="completed"
-													type="checkbox"
-													checked={completed}
-													onChange={toggleAssessmentCompletion(_id)}
-												/>
-											</Col>
-											<Col xs="5" sm="5" md="5" lg="5" xl="5">
-												<h4>{title}</h4>
-												<h5>{course.title}</h5>
-											</Col>
-											<Col xs="4" sm="4" md="4" lg="4" xl="4">
-												<p>{type}</p>
-												<p>
-													{ !end ?
-                                                        (moment(start, "MMMM Do, h:mm a"))
-                                                    : moment(start).startOf("day") !== moment(end).startOf("day") ?
-                                                        ( `${moment(start, "MMMM Do, h:mm a")} - ${moment(end, "MMMM Do, h:mm a")}`)
-                                                    : `${moment(start, "MMMM Do, h:mm a")} - ${moment(end, "h:mm a")}`
-													}
-												</p>
-											</Col>
-											<Col xs="2" sm="2" md="2" lg="2" xl="2">
-												<AssessmentEdit onClick={editAssessment(_id)}/>
-												<Button onClick={deleteAssessment(_id)}>
-													icon
-												</Button>
-											</Col>
-										</Row>
-									);
-								})}
-							/>
-						</Row>
-					</Col>
+					{ isMobile ? 
+						<MobileDashboard
+							meta={meta}
+							error={error}
+							classes={classes}
+							tasks={tasks}
+							assessments={assessments}
+							editClass={editClass}
+							toggleTaskCompletion={toggleTaskCompletion}
+							deleteClass={deleteClass}
+							editTask={editTask}
+							deleteTask={deleteTask}
+							editAssessment={editAssessment}
+							toggleAssessmentCompletion={toggleAssessmentCompletion}
+							deleteAssessment={deleteAssessment}
+						/>
+					: isTablet ? 
+						<TabletDashboard
+							meta={meta}
+							error={error}
+							classes={classes}
+							tasks={tasks}
+							assessments={assessments}
+							editClass={editClass}
+							toggleTaskCompletion={toggleTaskCompletion}
+							deleteClass={deleteClass}
+							editTask={editTask}
+							deleteTask={deleteTask}
+							editAssessment={editAssessment}
+							toggleAssessmentCompletion={toggleAssessmentCompletion}
+							deleteAssessment={deleteAssessment}
+						/>
+					: 	<DesktopDashboard
+							meta={meta}
+							error={error}
+							classes={classes}
+							tasks={tasks}
+							assessments={assessments}
+							editClass={editClass}
+							deleteClass={deleteClass}
+							editTask={editTask}
+							toggleTaskCompletion={toggleTaskCompletion}
+							deleteTask={deleteTask}
+							editAssessment={editAssessment}
+							toggleAssessmentCompletion={toggleAssessmentCompletion}
+							deleteAssessment={deleteAssessment}
+						/> 
+					}
 				</Row>
 			</>
 		);
 	};
 };
 
-const ClassEdit = Loadable({
-	loader: () => import(/* webpackChunkName: "ClassEdit" */ "../../reactors/ClassEdit"),
+const DesktopDashboard = Loadable({
+	loader: () => import(/* webpackChunkName: "DesktopDashboard" */ "../../templates/DesktopDashboard"),
 	loading: () => <div></div>,
 	delay: 300
 });
 
-const TaskNew = Loadable({
-	loader: () => import(/* webpackChunkName: "TaskNew" */ "../../reactors/TaskNew"),
+const MobileDashboard = Loadable({
+	loader: () => import(/* webpackChunkName: "MobileDashboard" */ "../../templates/MobileDashboard"),
 	loading: () => <div></div>,
 	delay: 300
 });
 
-const TaskEdit = Loadable({
-	loader: () => import(/* webpackChunkName: "TaskEdit" */ "../../reactors/TaskEdit"),
-	loading: () => <div></div>,
-	delay: 300
-});
-
-const AssessmentEdit = Loadable({
-	loader: () => import(/* webpackChunkName: "AssessmentEdit" */ "../../reactors/AssessmentEdit"),
+const TabletDashboard = Loadable({
+	loader: () => import(/* webpackChunkName: "TabletDashboard" */ "../../templates/TabletDashboard"),
 	loading: () => <div></div>,
 	delay: 300
 });
 
 const mapStateToProps = state => ({
 	meta: state.meta,
-	// isAuthenticated: state.auth.isAuthenticated,
 	error: state.error,
 	classes: state.classes,
 	task: state.task,
