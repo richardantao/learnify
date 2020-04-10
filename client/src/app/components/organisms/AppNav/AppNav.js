d2ad157262e45cd29be916f8edd4c3d03db12593
Richard Antao <richardmantao@gmail.com>
2020-04-10 18:38:34 -0400

    Converted component from class to function

--- client/src/app/components/organisms/AppNav/AppNav.js
+++ client/src/app/components/organisms/AppNav/AppNav.js
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom"; 
 
-import { 
-	faTachometerAlt, faCalendarAlt, faTasks, faUniversity
-} from "@fortawesome/free-solid-svg-icons";
+import { faTachometerAlt, faCalendarAlt, faTasks, faUniversity} from "@fortawesome/free-solid-svg-icons";
 
 /* Atoms */
 import Icon from "../../atoms/Icon";
@@ -13,62 +11,22 @@ import logo from "./learnify-min.png";
 
 import "./AppNav.scss";
 
-export default class AppNav extends Component {
-	state = {
-		active: ""
-	};
-
-	componentDidMount() {
-		this.setState({ });
-	};
-
-	componentDidUpdate(prevProps, prevState) {
-
-	};
-
-	setActive = active => {
-		this.setState({ active });
-	};
-
-	render() {
-		return ( 
-			<nav id="app-nav" role="navigation">
-				<Logo 
-					className="logo" 
-					href="/beta/dashboard" 
-					src={logo} 
-					alt="Learnify Logo" 
-				/>
-				<NavLink 
-					to="/beta/dashboard" 
-					className="btn" 
-					activeClassName="active-link"
-				>
-					<Icon icon={faTachometerAlt}/>
-				</NavLink>
-				<NavLink 
-					to="/beta/calendar" 
-					className="btn" 
-					activeClassName="active-link"
-				>
-					<Icon icon={faCalendarAlt}/>
-				</NavLink>
-				<NavLink 
-					to="/beta/academics" 
-					className="btn" 
-					activeClassName="active-link"
-
-				>
-					<Icon icon={faUniversity}/>
-				</NavLink>
-				<NavLink 
-					to="/beta/planner" 
-					className="btn" 
-					activeClassName="active-link"
-				>
-					<Icon icon={faTasks}/>
-				</NavLink>
-			</nav>
-		);
-	};
+export default () => {
+	return ( 
+		<nav id="app-nav" role="navigation">
+			<Logo className="logo" href="/beta/dashboard" src={logo} alt="Learnify Logo" />
+			<NavLink to="/beta/dashboard" className="btn" activeClassName="active-link">
+				<Icon icon={faTachometerAlt}/>
+			</NavLink>
+			<NavLink to="/beta/calendar" className="btn" activeClassName="active-link">
+				<Icon icon={faCalendarAlt}/>
+			</NavLink>
+			<NavLink to="/beta/academics" className="btn" activeClassName="active-link">
+				<Icon icon={faUniversity}/>
+			</NavLink>
+			<NavLink to="/beta/planner" className="btn" activeClassName="active-link">
+				<Icon icon={faTasks}/>
+			</NavLink>
+		</nav>
+	);
 };
