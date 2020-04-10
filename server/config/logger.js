6638c29307a3372c97ec3b8af89c25604087d986
Richard Antao <richardmantao@gmail.com>
2020-04-10 14:24:09 -0400

    Removed GCP stackdriver

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,12 +1,8 @@
 const winston = require("winston");
-const { LoggingWinston } = require("@google-cloud/logging-winston");
-
-const stackDriver = new LoggingWinston;
 
 module.exports = winston.createLogger({
     level: "info",
     transports: [
-      new winston.transports.Console(),
-      stackDriver
+      new winston.transports.Console()
     ]
 });
