9a4e78b0f1d8af64f49dc66a48de02c632dc4878
Richard Antao <richardmantao@gmail.com>
2020-04-10 18:10:12 -0400

    Added try-catch

--- server/api/controllers/errors.js
+++ server/api/controllers/errors.js
@@ -3,5 +3,11 @@ const logger = require("../../config/logger");
 module.exports = (req, res) => {
     const { error, errorInfo } = req.body;
 
-    return logger.error(error, errorInfo);
+    try {
+        logger.error(error, errorInfo);
+    } catch (e) {
+        logger.error(`An error occured while logging a client-side error: ${error}, ${errorInfo}`);
+    } finally {
+        return res.status(200).json({ message: "We apologize for the inconvenience. We take errors very seriously, and will be addressing this issue shortly. If the error persists, we encourage you to contact the admin" });
+    }
 };
