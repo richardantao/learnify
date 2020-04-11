3fba4611e61e3011e494604e630039a968309932
Richard Antao <richardmantao@gmail.com>
2020-04-11 00:47:58 -0400

    Added hook

--- client/src/app/components/atoms/MessageBoard/MessageBoard.js
+++ client/src/app/components/atoms/MessageBoard/MessageBoard.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from "reactstrap";
 
-export default ({ isOpen, toggle, header, body, footer }) => {
+export default ({ header, body, footer }) => {
+    const [isOpen, toggle] = useState(false);
+
     return (
-        <Modal isOpen={isOpen} toggle={toggle}>
-            <ModalHeader toggle={toggle}>{header}</ModalHeader>
+        <Modal isOpen={isOpen} toggle={() => toggle(!isOpen)}>
+            <ModalHeader toggle={() => toggle(!isOpen)}>{header}</ModalHeader>
             <ModalBody>
                 {body}
             </ModalBody>
